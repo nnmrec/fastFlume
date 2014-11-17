cfa12af77c37b5761ece412afe09d3c92870c42c
Danny Clay Sale <dsale@uw.edu>
2014-11-17 14:55:17 -0800

    clean up unused files and directories
    For most updated pre-processing scripts, see my APS 2014 folder

--- preprocess/parametersALM.py
+++
@@ -1,8 +0,0 @@
-# this script computes parameters for the Actuator Line Method, 
-# following the guidelines written by:
-#
-# Pankaj K. Jha, Matthew J. Churchfield, Patrick J. Moriarty, and Sven Schmitz
-# "Guidelines for Volume Force Distributions Within Actuator Line Modeling 
-# of Wind Turbines on Large-Eddy Simulation-Type Grids" 2013
-
-
