13154b3ec6a41bf7c1327b5ae943cce145b78d9d
Danny Clay Sale <dsale@uw.edu>
2015-02-18 11:26:52 -0800

    add Matlab script for post-processing

---
+++ fastFlume-PostProcessing.m
@@ -0,0 +1,448 @@
+%%
+clear all;
+close all;
+clc;
+
+%%
+% should add a system call to extract the TAR files before reading
+
+%%
+% data_directory  = 'C:\Users\danny-laptop\Downloads\APS_2014_temp\fastFlume_turbineOutput\turbineOutput-v4\turbineOutput';
+% data_directory  = '/mnt/data-RAID-10/danny/fastFlume__v4/turbineOutput';
+% restart_folders = {'0', '9.58', '10'};
+
+% data_directory  = 'C:\Users\danny-laptop\Downloads\APS_2014_temp\fastFlume_turbineOutput\turbineOutput-v5\turbineOutput';
+% % data_directory  = '/mnt/data-RAID-10/danny/fastFlume__v5/turbineOutput';
+% restart_folders = {'0'};
+
+% data_directory  = 'C:\Users\danny-laptop\Downloads\APS_2014_temp\fastFlume_turbineOutput\turbineOutput-v6\turbineOutput';
+% data_directory  = '/mnt/data-RAID-10/danny/fastFlume__v7/turbineOutput';
+% restart_folders = {'0'};
+
+% data_directory  = 'C:\Users\danny-laptop\Downloads\APS_2014_temp\fastFlume_turbineOutput\turbineOutput-v7\turbineOutput';
+% % data_directory  = '/mnt/data-RAID-10/danny/fastFlume__v7/turbineOutput';
+% restart_folders = {'0'};
+
+% data_directory  = '/mnt/data-RAID-10/danny/fastFlume-RC3_FullScale_mesh=Medium/turbineOutput';
+% restart_folders = {'0' '3.7'};
+
+data_directory  = '/mnt/data-RAID-10/danny/fastFlume-RC4_LabScale_mesh=medium/turbineOutput';
+restart_folders = {'0','0.9','3.1','4.14'};
+
+% NREL 5MW wind turbine
+% U_inf    = 8;         % free stream velocity
+% density  = 1.225;     % fluid density
+% RotorRad = 63;   
+% Pavail   = 5296610;   % rated mechanical power for the NREL 5MW turbine
+
+% Nick's lab scale hydrokinetic turbine
+U_inf    = 1.1;         % free stream velocity
+density  = 1000;        % fluid density
+RotorRad = 0.225;   
+Pavail   = 0.5*density*pi*RotorRad^2*U_inf^3;   % rated mechanical power for the NREL 5MW turbine
+
+% DOE RM1 full scale hydrokinetic turbine dual rotor
+% U_inf    = 1.9;         % free stream velocity
+% density  = 1025;        % fluid density
+% RotorRad = 10.0;   
+% Pavail   = 0.5*density*pi*RotorRad^2*U_inf^3;   % rated mechanical power
+
+
+numTurbines = 3;
+id_turbine = [0 1 2];
+
+% numTurbines = 2;
+% id_turbine = [0 1];
+
+% subset of data
+perStart = 0;   % end point - percent of time series
+perEnd   = 1;   % end point - percent of time series
+
+
+%%
+data_filename   = 'powerRotor';
+numCols         = 4;
+nHeaders        = 1;
+
+
+turbine_id = [];
+t          = [];
+dt         = [];
+RotorPower = [];
+% data = cell(1, numel(restart_folders));
+for n = 1:numel(restart_folders)
+    
+    datafile = [data_directory filesep restart_folders{n} filesep data_filename];
+    
+    fid = fopen(datafile, 'r');
+    if fid == -1
+        error(['ERROR: Could not locate and open file ' datafile]);
+    end
+
+    % skip the header lines
+    for j = 1:nHeaders
+        fgetl(fid);
+    end
+
+    % read the table of data
+%     data(n) = { textscan(fid, repmat('%f ', 1, numCols), 'CollectOutput', 1) };
+
+    tmp  = {textscan(fid, repmat('%f ', 1, numCols), 'CollectOutput', 0)};
+    data = cell2mat(tmp{:});
+    
+    turbine_id = [turbine_id; data(:,1)];
+    t          = [t; data(:,2)];
+    dt         = [dt; data(:,3)];
+    RotorPower = [RotorPower; data(:,4)];
+
+    % close the data file
+    fclose(fid);
+end
+
+
+% for n = 1:numel(restart_folders)
+%    % reconstruct into single time-series
+%    
+% end
+
+%% separate the data series by turbine identification
+
+% Protor = zeros(numel(unique(t)), numTurbines);
+for n = 1:numel(id_turbine)
+%     n
+    index       = turbine_id == id_turbine(n);
+%     size(index)
+    Protor(:,n) = RotorPower(index);
+%     Protor = RotorPower(index);
+
+    % filter the power, low pass filter
+%     Protor_filter(:,n) = 
+end
+
+time = t(:,1);
+time = time(turbine_id == id_turbine(1));
+
+%% only take subset of data
+
+Nsamples = numel(time);
+Nstart   = max(floor(Nsamples*perStart), 1);
+Nend     = max(ceil(Nsamples*perStart), Nsamples);
+
+time  =  time(Nstart:Nend, 1);
+Protor = Protor(Nstart:Nend, :);
+
+%% Plot the power
+figure('Name', 'Rotor Power', ...
+       'Color', 'white');
+
+% plot(time, Protor, 'LineWidth', 2)
+hold on;
+plot(time, Protor(:,1), '-b', 'LineWidth', 3)
+plot(time, Protor(:,2), '-k', 'LineWidth', 3)
+plot(time, Protor(:,3), '-r', 'LineWidth', 3)
+
+legend('turbine upstream','turbine middle','turbine downstream', ...
+       'Location','best');
+   
+title('rotor power', 'FontSize', 16)
+xlabel('time, t (s)', 'FontSize', 16);
+ylabel('rotor power, P (W)', 'FontSize', 16);
+   
+set(gca,'FontSize', 16)
+box on
+grid on
+
+%% Plot the efficiency
+figure('Name', 'Rotor Efficiency', ...
+       'Color', 'white');
+  
+% plot(time, Protor ./ Pavail, 'LineWidth', 2)
+hold on;
+plot(time, Protor(:,1) ./ Pavail, '-b', 'LineWidth', 3)
+plot(time, Protor(:,2) ./ Pavail, '-k', 'LineWidth', 3)
+plot(time, Protor(:,3) ./ Pavail, '-r', 'LineWidth', 3)
+
+legend('turbine upstream','turbine middle','turbine downstream', ...
+       'Location','best');
+   
+title('rotor efficiency', 'FontSize', 16);
+xlabel('time, t (s)', 'FontSize', 16);
+ylabel('rotor efficiency, Cp', 'FontSize', 16);
+   
+axis([time(1) time(end) 0 1]);
+set(gca,'FontSize', 16)
+box on
+grid on
+
+%% Plot the relative power
+figure('Name', 'Relative Rotor Power', ...
+       'Color', 'white');
+  
+relative_power = Protor ./ repmat(Protor(:,1), 1, numTurbines);
+% plot(time, relative_power, 'LineWidth', 2)
+hold on;
+plot(time, relative_power(:,1), '-b', 'LineWidth', 3)
+plot(time, relative_power(:,2), '-k', 'LineWidth', 3)
+plot(time, relative_power(:,3), '-r', 'LineWidth', 3)
+
+
+legend('turbine upstream','turbine middle','turbine downstream', ...
+       'Location','best');
+   
+title('Relative Rotor Power', 'FontSize', 16)
+xlabel('time, t (s)', 'FontSize', 16);
+ylabel('relative rotor power, P_{i} / P_{upstream}', 'FontSize', 16);
+   
+axis([time(1) time(end) 0 1])
+set(gca,'FontSize', 16)
+box on
+grid on
+
+%% the FFT with correct scaling
+% http://math.stackexchange.com/questions/636847/understanding-fourier-transform-example-in-matlab
+
+% signal = Protor(:,1);
+% Fs     = 1000;                    % Sampling frequency
+% 
+% L      = time(end) - time(1);       % Sample time
+% 
+% N      = numel(signal);
+% 
+% 
+% NFFT   = 2^nextpow2(L); % Next power of 2 from length of y
+% 
+% Y = fft(signal, NFFT)/Fs; % The Correct Scaling
+% f    = Fs/2*linspace(0, 1, NFFT/2+1)';
+
+%%
+% figure('Name', 'spectra of rotor power', ...
+%        'Color', 'white');
+%    
+% signal = Protor(:,1);
+% fs     = 10;                    % Sampling frequency
+% 
+% N        = numel(signal);
+% NFFT     = 2^nextpow2(L); % Next power of 2 from length of y
+% X_mags   = abs( fft(signal, NFFT) );
+% Y        = fft(signal, NFFT)/Fs; % The Correct Scaling
+% bin_vals = (0 : N-1)';
+% fax_Hz   = bin_vals*fs/N;
+% N_2      = ceil(N/2);
+% 
+% plot(fax_Hz(1:N_2), X_mags(1:N_2))
+% 
+% title('Single-sided Magnitude spectrum (Hertz)');
+% xlabel('Frequency (Hz)')
+% ylabel('Magnitude');
+% 
+% set(gca,'xscale','log');
+% set(gca,'yscale','log');
+% box on
+% grid on
+
+%% 
+% x  = Protor(:,1);
+% Fs = 10;
+% 
+% N = length(x);
+% xdft = fft(x);
+% xdft = xdft(1:N/2+1);
+% psdx = (1/(Fs*N)) * abs(xdft).^2;
+% psdx(2:end-1) = 2*psdx(2:end-1);
+% freq = 0:Fs/length(x):Fs/2;
+% 
+% plot(freq,10*log10(psdx))
+% grid on
+% title('Periodogram Using FFT')
+% xlabel('Frequency (Hz)')
+% ylabel('Power/Frequency (dB/Hz)')
+
+% T = time(end) - time(1);
+
+
+
+% NFFT = 2^nextpow2(N);        % Next power of 2 from length of input
+% 
+% p    = abs( fft(x) ) / (NFFT/2);
+% pp   = p(1:NFFT/2).^2;
+% freq = [0:NFFT/2-1] / T; 
+% 
+% semilogy(freq, pp);
+
+% signal = Protor(:,1);
+% N      = numel(signal);
+% 
+% X_mags   = abs( fft(signal) );
+% bin_vals = (0:N-1)';
+% fax_Hz = bin_vals*fs/N;
+% N_2 = ceil(N/2);
+% plot(fax_Hz(1:N_2), X_mags(1:N_2))
+% xlabel('Frequency (Hz)')
+% ylabel('Magnitude');
+% title('Single-sided Magnitude spectrum (Hertz)');
+% axis tight
+
+
+%% calculate the FFT
+% Fs   = 50;                   % sampling frequency
+Fs = floor( 1 / (max(time)/Nsamples) );
+
+N    = numel(time);
+NFFT = 2^nextpow2(N);        % Next power of 2 from length of input
+f    = Fs/2*linspace(0, 1, NFFT/2+1)';
+
+% fft_power_1 = fft(Protor(:,1), NFFT) ./ Fs;
+% fft_power_2 = fft(Protor(:,2), NFFT) ./ Fs;
+% fft_power_3 = fft(Protor(:,3), NFFT) ./ Fs;
+fft_power_1 = fft(Protor(:,1), NFFT) ./ N;   % absolute value of fft
+fft_power_2 = fft(Protor(:,2), NFFT) ./ N;
+fft_power_3 = fft(Protor(:,3), NFFT) ./ N;
+% fft_power = fft(Protor, NFFT) ./ N;
+
+% single-sided amplitude spectrum
+% mag_fft_power_1 = 2*abs( fft_power_1(1:NFFT/2+1) );
+% 
+% % Plot single-sided amplitude spectrum.
+% plot(f, mag_fft_power_1); 
+% title('Single-Sided Amplitude Spectrum of y(t)')
+% xlabel('Frequency (Hz)')
+% ylabel('|Y(f)|')
+
+% power spectral density
+psd_power_1 = abs( fft_power_1(1:NFFT/2+1) ) ./ (f/2);
+psd_power_2 = abs( fft_power_2(1:NFFT/2+1) ) ./ (f/2);
+psd_power_3 = abs( fft_power_3(1:NFFT/2+1) ) ./ (f/2);
+% psd_power = 2*abs( fft_power(1:NFFT/2+1,:) ) ./ f;
+
+% plot the FFT
+figure('Name', 'spectra of rotor power', ...
+       'Color', 'white');
+   
+hold on;
+% Plot single-sided amplitude spectrum.
+% plot(f, 2*abs(fft_power_1(1:NFFT/2+1)), 'LineWidth', 2) 
+% plot(f, 2*abs(fft_power_2(1:NFFT/2+1)), 'LineWidth', 2) 
+% plot(f, 2*abs(fft_power_3(1:NFFT/2+1)), 'LineWidth', 2) 
+plot(f, psd_power_1, '-b', 'LineWidth', 2) 
+plot(f, psd_power_2, '-k','LineWidth', 2) 
+plot(f, psd_power_3, '-r','LineWidth', 2)
+% plot(f, psd_power, 'LineWidth', 2)
+
+title('spectra of rotor power', 'FontSize', 16)
+xlabel('frequency, f (Hz)', 'FontSize', 16);
+ylabel('rotor power, PSD (W^2/Hz)', 'FontSize', 16);
+
+legend('turbine upstream','turbine middle','turbine downstream', ...
+       'Location','best');
+
+set(gca,'xscale','log');
+set(gca,'yscale','log');
+set(gca,'FontSize', 16)
+box on
+grid on
+
+
+%%
+% INPUT VARIABLES:
+%   x - Time series, [vector]
+%   dt - Sampling Rate, [scalar]
+%   win - Window, one of: 
+%       'hanning', 'hamming', 'boxcar'
+%   Nsg - Number of Segments (>=1)
+%   pnv - Percentage Noverlap of Segments (0-100)
+%   Nb - Band Averaging, number of bands to average
+%   Wn - Cut-Off frequencies, used for filtering
+%   ftype - Type of filter, 'high', 'low' or 'stop'
+%   n - Number of coefficients to use in
+%   the Butterworth filter
+
+% x       = Protor(:,1);
+% dt      = 10;                    % Sampling frequency
+% w       = 'hanning';
+% Nsg     = 2;
+% pnv     = 50;
+% Wn      = dt;
+% ftype   = 'high';
+% n       = 1;
+
+% [psdf,conf,f] = specwelch(x,dt,w,Nsg,pnv,Wn,ftype,n)
+
+
+
+
+%%
+% x  = Protor(:,1);
+% Fs     = 10;                    % Sampling frequency
+% 
+% N    = length(x);
+% xdft = fft(x);
+% xdft = xdft(1:N/2+1);
+% psdx = (1/(Fs*N)) * abs(xdft).^2;
+% psdx(2:end-1) = 2*psdx(2:end-1);
+% freq = 0:Fs/length(x):Fs/2;
+% 
+% plot(freq,10*log10(psdx))
+% grid on
+% title('Periodogram Using FFT')
+% xlabel('Frequency (Hz)')
+% ylabel('Power/Frequency (dB/Hz)')
+
+%% plot the filtered power
+
+load bostemp
+days = (1:31*24)/24;
+plot(days, tempC), axis tight;
+ylabel('Temp (\circC)');
+xlabel('Time elapsed from Jan 1, 2011 (days)');
+title('Logan Airport Dry Bulb Temperature (source: NOAA)');
+
+% In its simplest form, a moving average filter of length N takes the 
+% average of every N consecutive samples of the waveform.
+% To apply a moving average filter to each data point, we construct our 
+% coefficients of our filter so that each point is equally weighted and 
+% contributes 1/24 to the total average. This gives us the average 
+% temperature over each 24 hour period.
+hoursPerDay = 24;
+coeff24hMA = ones(1, hoursPerDay)/hoursPerDay;
+
+avg24hTempC = filter(coeff24hMA, 1, tempC);
+plot(days, [tempC avg24hTempC]);
+legend('Hourly Temp','24 Hour Average (delayed)','location','best');
+ylabel('Temp (\circC)');
+xlabel('Time elapsed from Jan 1, 2011 (days)');
+title('Logan Airport Dry Bulb Temperature (source: NOAA)');
+
+
+%%
+figure
+
+k = 3
+samplesPerSecond = ceil( Nsamples * 100);
+coeff_0p5hz_MA = ones(1, samplesPerSecond)/samplesPerSecond;
+
+Power_filter = filter(coeff24hMA, 1, Protor(:,k));
+plot(time, [Protor(:,k) Power_filter]);
+% legend('upstream', ...
+%        'middle', ...
+%        'downstream', ...
+%        'location','best');
+ylabel('rotor power, P (W)');
+xlabel('time, t (s)');
+title('Filtered Rotor Power');
+
+
+
+
+%% estimate time for fastFlume to complete
+% dw          = 0.02;     % write interval
+% T           = 20;       % total simulation time in seconds
+% timePerIter = mean([46 39 36 41 42 43 49 54 60 53].*60);    % minutes converted to seconds
+% iters       = T / dw;    % number of write iterations
+% 
+% time_to_wait = fprintf(1, 'time_to_wait = %g hours \n', iters*timePerIter/3600);
+
+
+
+
+
+
