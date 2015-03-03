5ae13a0633ca07634f14eb62b630f24ddd999bd2
danny <danny@Weathertop.site>
2015-03-02 16:18:31 -0800

    update script for making movies using FFMPEG

--- make-movie.sh
+++ make-movie.sh
@@ -1,7 +1,19 @@
-# webm
+
+# just run this script from a directory containing a series of images 
+# convert the series of PNG images to GIF images
+mogrify -format gif *.png
  
-# ffmpeg -i input.ext -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf 
-"scale=trunc(in_w/2)*2:trunc(in_h/2)*2" output.webm
+# now combine the GIF images into a single animated GIF, using compression
+gifsicle --colors=256 --delay=4 --loopcount=0 --dither -O3 *.gif > output_gifsicle.gif
+
+# Also, this combo of commands for FFMPEG works okay
+ffmpeg -framerate 50 -i fastFlume-RC5__%04d.png -s:v 1920x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p movie-v1-50fps.mp4
+
+
+
+## the below lines are for reference only, and commands are broken, or not quite right
+ 
+# ffmpeg -i input.ext -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" output.webm
  
 # This will turn input.ext into output.webm.
  
@@ -12,12 +24,4 @@
  
 # INPUT='fastDuct_v3_.png'
  
-# ffmpeg -i fastDuct_v3_.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf 
-"scale=trunc(in_w/2)*2:trunc(in_h/2)*2" zzzzz_fastDuct.webm
-
-# just run this script from a directory containing a series of images 
-# convert the series of PNG images to GIF images
-mogrify -format gif *.png
- 
-# now combine the GIF images into a single animated GIF, using compression
-gifsicle --colors=256 --delay=4 --loopcount=0 --dither -O3 *.gif > output_gifsicle.gif
+# ffmpeg -i fastDuct_v3_.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" zzzzz_fastDuct.webm
