90f0d7b94df2d1c175c5e071256056df09f42609
Danny Clay Sale <dsale@uw.edu>
2015-07-28 17:39:01 -0700

    update parameters for mesh=medium3

--- run-master-node-sysCall.sh
+++ run-master-node-sysCall.sh
@@ -10,23 +10,24 @@ then
  	
 
  	# reconstruct before sampling or converting
- 	reconstructPar -latestTime
+ 	reconstructPar -latestTime 2>&1 | tee log.reconstructPar
 
  	# compute any additional fields that would be sampled
- 	vorticity -latestTime
-	enstrophy -latestTime
-	foamCalc components vorticity -latestTime
-	foamCalc components U -latestTime
-	foamCalc components UMean -latestTime
-	Lambda2 -latestTime
-	pPrime2 -latestTime
-	uprime -latestTime
+ # 	vorticity -latestTime
+	# enstrophy -latestTime
+	# foamCalc components vorticity -latestTime
+	foamCalc components U -latestTime 2>&1 | tee log.components-U
+	foamCalc components UMean -latestTime 2>&1 | tee log.components-Umean
+	# Lambda2 -latestTime
+	# pPrime2 -latestTime
+	uprime -latestTime 2>&1 | tee log.uprime
+	foamCalc components uprime -latestTime 2>&1 | tee log.components-uprime
 
 	## convert OpenFOAM format to VTK files
-	foamToVTK -latestTime
+	foamToVTK -latestTime 2>&1 | tee log.foamToVTK
 
-	## sample the fields
-	sample -latestTime
+	## sample the fields (this hangs using planes ... change to point cloud method requiring )
+	sample -latestTime 2>&1 | tee log.sample
 
 fi
 
