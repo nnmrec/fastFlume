7d35e0fdff51095cfacdeeeca6b2f698ac41496a
Danny Clay Sale <dsale@uw.edu>
2015-08-12 16:44:13 -0700

    update to reinitialize-FAST

--- utilities/reinitialize-FAST.sh
+++ utilities/reinitialize-FAST.sh
@@ -7,7 +7,7 @@ cd ${0%/*} || exit 1 # run from this directory
 
 
 ## The file to modify
-fileFAST=../primary.fst
+fileFAST=./../primary.fst
 
 
 ## Find the name of last time directory where OpenFOAM stopped
@@ -15,36 +15,72 @@ fileFAST=../primary.fst
 ## sort the directory names and list the time directories
 
 ## or find the the most recently modified file
-lastTime=$(ls -t ./../turbineOutput/ | head -n 1)
-echo $lastTime
+lastTimeFAST=$(ls -t ./../turbineOutput/ | head -n 1)
+lastTimeFOAM=$(ls -t ./../processor0/ | head -n 1)
+echo $lastTimeFAST
+echo $lastTimeFOAM
 
 ## now find the lastTime directory of the FAST output files
 ## (note: the rotorSpeed and azimuth files share same format)
-lastDir="./../turbineOutput/$lastTime/"
+lastDir="./../turbineOutput/$lastTimeFAST/"
 echo $lastDir
 
-## now search the second column for the line of previous time step
-
+## now read the second column for the line of previous time step
 timesFAST=$(awk '{print $2}' $lastDir/azimuth)
+echo $timesFAST
 
+## convert the string into an array
+times=($timesFAST)
+echo $times
+
+## cut off the first element of the array (which is the variable name)
+times="${times[@]:1}"
+times=${times[@]:1}
+echo $times
+echo ${times[1]}
+echo ${times[2]}
+
+newTimes="$times"
+echo $newTimes
+echo ${newTimes[1]}
+
+my_array=(red orange green)
+value='green'
+
+for i in "${!my_array[@]}"; do
+   if [[ "${my_array[$i]}" = "${value}" ]]; then
+       echo "${i}";
+   fi
+done
+
+## now find the index of the time value where OpenFOAM will restart
+
+# for i in "${times[@]}"
+# 	do
+#    	echo "$i"
+#    	# or do whatever with individual element of the array
+# done
+
+
+## Read the last known blade azimuth, and overwite to FAST input file
+# fileName=./turbineOutput/$lastDir/azimuth
+# lastLine=$(tac $fileName | egrep -m 1 .)
+# lastWord=$(echo $lastLine | awk '{ print $(NF) }')
+# lineNumb=72
+# ./replace-line.sh $lineNumb "$lastWord    Azimuth     - Initial azimuth angle for blade 1 (degrees)" $fileFAST > replace.$fileFAST
+# mv replace.$fileFAST $fileFAST
 
 
 
 ## Read the last known rotor speed, and overwite to FAST input file
-fileName=./turbineOutput/$lastDir/rotSpeed
-lastLine=$(tac $fileName | egrep -m 1 .)
-lastWord=$(echo $lastLine | awk '{ print $(NF) }')
-lineNumb=73
-./replace-line.sh $lineNumb "$lastWord    RotSpeed    - Initial or fixed rotor speed (rpm)" $fileFAST > replace.$fileFAST
-mv replace.$fileFAST $fileFAST
+# fileName=./turbineOutput/$lastDir/rotSpeed
+# lastLine=$(tac $fileName | egrep -m 1 .)
+# lastWord=$(echo $lastLine | awk '{ print $(NF) }')
+# lineNumb=73
+# ./replace-line.sh $lineNumb "$lastWord    RotSpeed    - Initial or fixed rotor speed (rpm)" $fileFAST > replace.$fileFAST
+# mv replace.$fileFAST $fileFAST
 
 
 
-## Read the last known blade azimuth, and overwite to FAST input file
-fileName=./turbineOutput/$lastDir/azimuth
-lastLine=$(tac $fileName | egrep -m 1 .)
-lastWord=$(echo $lastLine | awk '{ print $(NF) }')
-lineNumb=72
-./replace-line.sh $lineNumb "$lastWord    Azimuth     - Initial azimuth angle for blade 1 (degrees)" $fileFAST > replace.$fileFAST
-mv replace.$fileFAST $fileFAST
+
 
