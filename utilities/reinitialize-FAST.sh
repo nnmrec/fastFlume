7ff4c4ba585c7742aad464fcef34fdb59f40bd33
Danny Clay Sale <sale.danny@gmail.com>
2015-08-15 22:08:05 -0700

    reinitialze FAST works for multiple turbines now

--- utilities/reinitialize-FAST.sh
+++ utilities/reinitialize-FAST.sh
@@ -4,41 +4,32 @@
 ## if OpenFOAM restarts from a previous timestep FAST should also
 ## reinitialize using previous time values
 ##
-## To-Do: this probably only works with a single turbine, if multiple
-##        turbines are defined the format of FAST output files is different
 
 
-## The file to modify
+## The files to modify
 cd ..
-fileFAST=$PWD/primary.fst
-echo $fileFAST
+fileFAST_MAIN=$PWD/primary.fst
+fileFAST_FOAM=$PWD/constant/turbineArrayProperties
 cd -
+
 
 ## Find the name of last time directory where OpenFOAM and FAST stopped
 ## these are the most recently modified files
 lastTimeFAST=$(ls -t ./../turbineOutput/ | head -n 1)
 lastTimeFOAM=$(ls -t ./../processor0/ | head -n 1)
 
-echo $lastTimeFAST
-echo $lastTimeFOAM
 
 ## now find the lastTime directory of the FAST output files
 ## (note: the rotorSpeed and azimuth files share same format)
 lastDirFAST=$(realpath ./../turbineOutput/$lastTimeFAST/)
 
-echo $lastDirFAST
 
-## now read the second column of the file
+## now read the columns for Time and Azimuth
 timesFAST=$(awk '{print $2}' $lastDirFAST/azimuth)
 azimuth=$(awk '{print $4}' $lastDirFAST/azimuth)
-
-
 ## convert to list so we can refer to elements using index
 timesFAST=($timesFAST)
 azimuth=($azimuth)
-
-echo $timesFAST
-echo $azimuth
 
 
 ## find the index of matching element in list
@@ -48,27 +39,33 @@ for ((i=0; i < ${#timesFAST[@]}; ++i)); do
     fi
 done
 
-echo $index
-
 
 ## define the new intital conditions to restart with
 init_azimuth=${azimuth[$index]}
 
-echo $init_azimuth
 
 ## Read the last known blade azimuth, and overwite to FAST input file
-# fileName=./turbineOutput/$lastDir/azimuth
-# lastLine=$(tac $fileName | egrep -m 1 .)
-# lastWord=$(echo $lastLine | awk '{ print $(NF) }')
-lineNumb=72
-./overwrite-line.sh $lineNumb "$init_azimuth    Azimuth     - Initial azimuth angle for blade 1 (degrees), using Azimuth from previous OpenFOAM timestep of t = asdf" $fileFAST > $fileFAST.replace
-mv $fileFAST.replace $fileFAST
-
+./overwrite-line.sh 72 "$init_azimuth    Azimuth     - Initial azimuth angle for blade 1 (degrees), using Azimuth from previous OpenFOAM timestep of t = $lastTimeFOAM" $fileFAST_MAIN > $fileFAST_MAIN.replace
+mv $fileFAST_MAIN.replace $fileFAST_MAIN
 
 
 ## also need to overwrite in the file constant/turbineArrayProperties
-## need to search for the line (or multiple lines) containing the "Azimuth" keyword
+## need to search for the line (or multiple lines for each turbine) containing the "Azimuth" keyword
+## find the line number where azimuth is initialized (this returns all line numbers)
+lineNumbers=$(grep -n 'Azimuth' $fileFAST_FOAM)
 
-replace line with
 
-"Azimuth   $init_azimuth;   // using Azimuth from previous OpenFOAM timestep of t = asdf"
+## only read the numbers, the first words separate by
+lines=$(echo "${lineNumbers[@]}" | awk -F":" '{print $1}')
+## convert to list so we can refer to elements using index
+lines=($lines)
+
+
+## replace the line number within each turbine's dictionary
+for ((i=0; i < ${#lines[@]}; ++i)); do
+
+    ./overwrite-line.sh ${lines[$i]} "    Azimuth        $init_azimuth;         // Initial azimuth angle for blade 1 (degrees), using Azimuth from previous OpenFOAM timestep of t = $lastTimeFOAM" $fileFAST_FOAM > $fileFAST_FOAM.replace
+    
+    mv $fileFAST_FOAM.replace $fileFAST_FOAM
+done
+
