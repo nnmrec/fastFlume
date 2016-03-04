d720eb3def9c4f042cf24c6781ba495f31f70f1a
Danny Clay Sale <dsale@uw.edu>
2016-03-03 17:52:51 -0800

    using ffmpeg to create webm movies

--- utilities/make-movie.sh
+++ utilities/make-movie.sh
@@ -1,13 +1,24 @@
+# just run this script from a directory containing a series of images 
+
+# get the base filename
+BASE_FILENAME=fastFlume__
+# try to improve the quality
+ffmpeg -i $BASE_FILENAME%04d.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" fastFlume.webm
+
+
+
+# OLD STUFF (just for reference)
 
 # just run this script from a directory containing a series of images 
 # convert the series of PNG images to GIF images
-mogrify -format gif *.png
- 
+# mogrify -format gif *.png 
 # now combine the GIF images into a single animated GIF, using compression
-gifsicle --colors=256 --delay=4 --loopcount=0 --dither -O3 *.gif > output_gifsicle.gif
+# gifsicle --colors=256 --delay=4 --loopcount=0 --dither -O3 *.gif > output_gifsicle.gif
 
 # Also, this combo of commands for FFMPEG works okay
-ffmpeg -framerate 50 -i fastFlume-RC5__%04d.png -s:v 1920x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p movie-v1-50fps.mp4
+
+# ffmpeg -framerate 50 -i $BASE_FILENAME%04d.png -s:v 1920x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p movie-v1-50fps.mp4
+# ffmpeg -framerate 50 -i fastFlume-RC5__%04d.png -s:v 1920x1080 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p movie-v1-50fps.mp4
 
 
 
@@ -24,4 +35,16 @@ ffmpeg -framerate 50 -i fastFlume-RC5__%04d.png -s:v 1920x1080 -c:v libx264 -pro
  
 # INPUT='fastDuct_v3_.png'
  
-# ffmpeg -i fastDuct_v3_.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" zzzzz_fastDuct.webm
+
+# ffmpeg -i $BASE_FILENAME%04d.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" zzzzz_fastDuct.webm
+# ffmpeg -i fastFlume_TipLossON_turbInlet_mps0p9_tsr7p00000.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" zzzzz_fastDuct.webm
+# cat *.png | ffmpeg -f image2pipe -r 50 -vcodec png -i - fastFlume_mps0p9_tsr7p0.webm
+# cat *.png | ffmpeg -f image2pipe -framerate 50 -vcodec png -i - fastFlume_mps0p9_tsr7p0.webm
+# ffmpeg -i input_file.avi -codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10 -qmax 42 -maxrate 500k -bufsize 1000k -threads 4 -vf scale=-1:480 -codec:a libvorbis -b:a 128k output.webm
+# cat *.png | ffmpeg -c:v libvpx-vp9 -crf 10 -b:v 0 -f image2pipe -framerate 50 -vcodec png -i - fastFlume_mps0p9_tsr7p0.webm
+
+# this works but quality is bad
+# cat *.png | ffmpeg -f image2pipe -framerate 50 -vcodec png -i - fastFlume_mps0p9_tsr7p0.webm
+
+# try to improve the quality
+ffmpeg -i $BASE_FILENAME%04d.png -c:v libvpx -c:a libvorbis -pix_fmt yuv420p -quality good -b:v 2M -crf 5 -vf "scale=trunc(in_w/2)*2:trunc(in_h/2)*2" fastFlume_mps0p9_tsr7p0.webm
